936db64cb94f90274ad13900116db374914e7cef
maklian <mark.aklian@gmail.com>
2016-10-26 02:12:22 +0000

    rm creds

--- run_docker_halo_instance.sh
+++ run_docker_halo_instance.sh
@@ -3,5 +3,5 @@
 cid=$(docker run -it -d ubtestworun-halo /bin/bash | cut -c 1-12)
 echo $cid
 sleep 5
-docker exec -it -d $cid /opt/cloudpassage/bin/configure --agent-key=5eb42703aaf007b796067c07bae787e6 --tag=docker
+docker exec -it -d $cid /opt/cloudpassage/bin/configure --agent-key=XXXXXXXXXXXX --tag=docker
 docker exec -it -d $cid /etc/init.d/cphalod start
